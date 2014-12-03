6dddf4dcda8d9c0299a1b98469231d867d726a58
CCXYJ\徐宇健 <18735125591@163.com>
2014-12-03 14:19:58 +0800

    test提交

--- src/com/zhensan/client/MainActivity.java
+++ src/com/zhensan/client/MainActivity.java
@@ -23,6 +23,8 @@ public class MainActivity extends Activity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+        
+        System.out.println("呵呵呵");
     }
 
 
